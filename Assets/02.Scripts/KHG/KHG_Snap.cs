bb93dadac1fe525b649aef23a8014d4b95dbf367
HyunGoo Kang <sg00001@naver.com>
2021-09-15 18:14:02 +0900

    transperent

--- Assets/02.Scripts/KHG/KHG_Snap.cs
+++ Assets/02.Scripts/KHG/KHG_Snap.cs
@@ -346,7 +346,7 @@ public class KHG_Snap : MonoBehaviour
         band2.enabled = true;
         for (int i = 0; i < 11; i++)
         {
-            Debug.Log("a");
+
             foreach (Material mat in bandMats)
             {
                 mat.color = new Color(mat.color.r, mat.color.g, mat.color.b, 0.1f * i);
