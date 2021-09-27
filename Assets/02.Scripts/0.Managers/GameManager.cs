c9b46b889aa5a83fa9d634691c7af09f4ab16a87
Jumi Oh <ojui385@gmail.com>
2021-09-27 17:14:08 +0900

    "Ward - Injection" - Exit Menu

--- Assets/02.Scripts/0.Managers/GameManager.cs
+++ Assets/02.Scripts/0.Managers/GameManager.cs
@@ -2,29 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance = null;
 
+    public bool isMenu = false;
+    public Timer timer;
+    public GameObject ExitMenu;
+
     #region SINGLETON
     void Awake()
     {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(this.gameObject);
-        }
-        else if (instance != this)
-        {
-            Destroy(this.gameObject);
-        }
+        instance = this;
     }
 
     #endregion
 
     private void Start()
     {
+    }
+
+    private void Update()
+    {
+        // A 버튼 누름 && 메뉴 꺼져있음
+        if (OVRInput.GetDown(OVRInput.Button.One) && !isMenu)
+        {
+            isMenu = true;
+
+            // Timer Stop
+            timer.timerOn = false;
+
+            // UI On
+            ExitMenu.SetActive(true);
+        }
+
+        // A 버튼 누름 && 메뉴 켜져있음
+        if (OVRInput.GetDown(OVRInput.Button.One) && isMenu)
+        {
+            // Go to Lobby
+            SceneManager.LoadScene("WatingRoom");
+        }
+
+        // B 버튼 누름 && 메뉴 켜져있음
+        if (OVRInput.GetDown(OVRInput.Button.Two) && isMenu)
+        {
+            isMenu = false;
+
+            // Timer Start
+            timer.timerOn = true;
+
+            // UI Off
+            ExitMenu.SetActive(false);
+        }
     }
 
 
