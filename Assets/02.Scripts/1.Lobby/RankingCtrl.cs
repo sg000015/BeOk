7a0a002ba4d8f3bfcd3b11a833996d2fb28e2748
Gyubong Bang <velpote@gmail.com>
2021-10-01 10:04:34 +0900

    주사씬 랭킹 UI 완성1

---
+++ Assets/02.Scripts/1.Lobby/RankingCtrl.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RankingCtrl : MonoBehaviour
+{
+    public GameManager_Lobby GM;
+
+    public GameObject NickNamePanel;
+    public GameObject RankingPanel;
+    public TMP_Text text;
+    GameObject prevPanel;
+    GameObject curPanel;
+    public void OnClickKey(string str)
+    {
+        if (SW)
+            text.text += str.ToUpper();
+        else
+            text.text += str.ToLower();
+
+    }
+    bool SW = false;
+     public void OnClickShift()
+    {
+        Transform keyboard = NickNamePanel.transform.GetChild(0);
+        SW = !SW;
+        foreach (TMP_Text text in keyboard.GetComponentsInChildren<TMP_Text>())
+        {
+            Debug.Log(text.text);
+            if (text.text == "Enter" ||
+                text.text == "Space" ||
+                text.text == "Delete")
+            {
+
+            }
+            else
+            {
+
+                if (SW)
+                {
+                    text.text = text.text.ToUpper();
+                }
+                else
+                {
+                    text.text = text.text.ToLower();
+                }
+            }
+        }
+    }
+    public void OnClickDelete()
+    {
+        text.text = " ";
+    }
+    public void OnClickSpace()
+    {
+        text.text += " ";
+    }
+
+    public void OnClickEnterBtn()
+    {
+
+        prevPanel = NickNamePanel;
+        curPanel = RankingPanel;
+        ShowAndHideUI();
+        //!
+        GM.SetNickname();
+
+    }
+    public void ShowAndHideUI(bool direction = true)
+    {
+        prevPanel.SetActive(!direction);
+        curPanel.SetActive(direction);
+    }
+}
